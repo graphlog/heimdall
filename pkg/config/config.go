a83da366410e7b07b040b6a1c52da153b8ab956a
Roger King <rogr.king@gmail.com>
2019-12-01 21:35:56 -0500

    feat: adding amqp and clickhouse config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,13 +7,23 @@ type ServerConfig struct {
 }
 
 type Config struct {
-	Server *ServerConfig
+	Server     *ServerConfig
+	DataStores *DS
+}
+
+type DS struct {
+	AMQPURI       string
+	ClickHouseURI string
 }
 
 func NewConfig() *Config {
 	return &Config{
 		Server: &ServerConfig{
 			Port: envOrDefault("PORT", "7000"),
+		},
+		DataStores: &DS{
+			AMQPURI:       envOrDefault("AMQP_URI", "amqp://localhost:5672"),
+			ClickHouseURI: envOrDefault("CLICKHOUSE_URI", "tcp://127.0.0.1:9000?debug=true"),
 		},
 	}
 }
